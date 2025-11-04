a5e680b203f38caac1074ffb17b70d6698e50635
Zehan <zehan.kareem@gmail.com>
2025-11-05 03:32:02 +0600

    update(iamzehan): Wrapped Nav List items with Link

--- src/components/Navbar/nav-links.tsx
+++ src/components/Navbar/nav-links.tsx
@@ -10,24 +10,26 @@ export default function NavLinks({ click }: { click?: () => void }) {
   return (
     <>
       {links.map((link, index) => {
-        const Icon = link.icon; // ✅ Capitalized to use as component
+        const Icon = link.icon;
+
         return (
-          <li
+          <Link
             key={index}
+            href={link.href}
+            prefetch={true}
+            onClick={() => click?.()}
             className={clsx(
-              "w-full px-4 py-2 md:rounded transition-colors duration-300 flex gap-4 items-center justify-between px-10 md:px-2 md:justify-center md:gap-2",
+              "w-full px-10 md:px-2 py-2 md:rounded transition-colors duration-300 flex gap-4 md:gap-2 items-center justify-between md:justify-center",
               {
                 "text-blue-600 font-bold bg-blue-300": pathname === link.href,
-                "text-blue-950 font-bold hover:text-lime-600 hover:cursor-pointer": pathname !== link.href,
+                "text-blue-950 font-bold hover:text-lime-600 hover:cursor-pointer":
+                  pathname !== link.href,
               }
             )}
-            onClick={() => click?.()}
           >
-            <Icon fontSize="small" /> {/* ✅ Render correctly */}
-            <Link className="h-full w-full text-left" href={link.href} onClick={() => click?.()}>
-              {link.label}
-            </Link>
-          </li>
+            <Icon fontSize="small" />
+            <span className="w-full">{link.label}</span>
+          </Link>
         );
       })}
     </>
