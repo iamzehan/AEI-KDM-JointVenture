8a3026b8b4fc618cb1153fffa35459d8aaf7cdf5
Zehan <zehan.kareem@gmail.com>
2025-11-05 03:26:23 +0600

    update(iamzehan): Added icons to the Navbar util

--- src/lib/nav-links.ts
+++ src/lib/nav-links.ts
@@ -1,22 +1,32 @@
+// lib/nav-links.ts
+import * as icons from "@mui/icons-material";
+import { SvgIconComponent } from "@mui/icons-material";
+
 interface NavLink {
-  href: string
-  label: string
+  href: string;
+  label: string;
+  icon: SvgIconComponent; 
 }
+
 export const links: NavLink[] = [
   {
     label: "Home",
     href: "/",
+    icon: icons.Home,
   },
   {
     label: "Blog",
     href: "/blogs",
+    icon: icons.Notes,
   },
   {
     label: "Projects",
     href: "/projects",
+    icon: icons.Engineering,
   },
   {
     label: "About",
     href: "/about",
+    icon: icons.BusinessCenter,
   },
 ];
