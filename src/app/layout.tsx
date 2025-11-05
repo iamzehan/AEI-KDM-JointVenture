924e4304ead6e0eb7f99a87a15845e1038dffc06
Zehan <zehan.kareem@gmail.com>
2025-11-06 05:45:52 +0600

    refactor!(iamzehan): Renamed component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/nav-bar";
-import Chat from "@/components/chat/chat";
+import Chat from "@/components/Chat/chat";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
