04b1ec7cba168b5d81f25e11b955f81622465b11
Zehan <zehan.kareem@gmail.com>
2025-11-05 07:16:21 +0600

    feat(iamzehan): Added text to heading

---
+++ src/lib/header.ts
@@ -0,0 +1,8 @@
+interface Text{
+    eng: string;
+    ban: string;
+}
+export const text:Text = {
+    eng: "Powering Bangladesh’s Future: A New Era of Energy!",
+    ban: "বাংলাদেশের ভবিষ্যৎ শক্তির জাগরণ: নতুন একটি যুগের সূচনা!"
+}
