7329c2425be578ac165eaa1322aa9a176bbfa925
Zehan <zehan.kareem@gmail.com>
2025-11-05 08:26:02 +0600

    update(iamzehan): Minor style changes

--- src/components/home/Header/heading.tsx
+++ src/components/home/Header/heading.tsx
@@ -13,8 +13,8 @@ const notoSansBengali = Noto_Sans_Bengali({
 
 export default function Heading() {
   return (
-    <div className="md:absolute z-12 h-fit w-full top-0 left-0 bg-zinc-950 md:top-auto md:bottom-0 md:left-0 p-3 flex flex-col md:backdrop-blur-xs md:bg-zinc-950/60">
-      <p className="text-2xl text-lime-600 md:text-white md:text-shadow-lg font-bold">
+    <div className="md:absolute z-12 h-fit w-full top-0 left-0 md:top-auto md:bottom-0 md:left-0 p-3 flex flex-col md:backdrop-blur-xs md:bg-zinc-950/60">
+      <p className="text-2xl text-sky-600 md:text-white md:text-shadow-lg font-bold">
         {text.eng}
       </p>
       <p
