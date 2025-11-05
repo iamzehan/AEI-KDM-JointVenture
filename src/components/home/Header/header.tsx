20d4a2031ba951325ca3538e196d87a44d5bb0f9
Zehan <zehan.kareem@gmail.com>
2025-11-05 09:12:24 +0600

    update(iamzehan): Minor design changes

--- src/components/home/Header/header.tsx
+++ src/components/home/Header/header.tsx
@@ -2,12 +2,11 @@
 import Carousel from "./carousel";
 import Heading from "./heading";
 
-export default function Header() {
-
+export default function Header({font}:{font:{className:string}}){
   return (
     <div className="relative flex flex-col h-full">
-      <Carousel/>
-      <Heading/>
+      <Carousel />
+      <Heading  font={font}/>
     </div>
   );
 }
--- src/components/home/Header/heading.tsx
+++ src/components/home/Header/heading.tsx
@@ -1,17 +1,10 @@
 'use client'
-import { Noto_Sans_Bengali } from "next/font/google";
+
 import clsx from "clsx";
 import { text } from "@/lib/header";
 
-
-const notoSansBengali = Noto_Sans_Bengali({
-  weight: ["400", "700"], // Specify desired weights
-  subsets: ["bengali"], // Crucial for Bangla character support
-  variable: "--font-noto-sans-bengali", // Optional CSS variable
-  display: "swap",
-});
-
-export default function Heading() {
+export default function Heading({font}: {font:{className:string}}) {
+  
   return (
     <div className="md:absolute z-12 h-fit w-full top-0 left-0 md:top-auto md:bottom-0 md:left-0 p-3 flex flex-col md:backdrop-blur-xs md:bg-zinc-950/60">
       <p className="text-2xl text-sky-600 md:text-white md:text-shadow-lg font-bold">
@@ -19,7 +12,7 @@ export default function Heading() {
       </p>
       <p
         className={clsx(
-          [notoSansBengali.className],
+        [font.className],
           ["text-blue-400 md:text-xl md:text-white md:text-shadow-lg"]
         )}
       >
