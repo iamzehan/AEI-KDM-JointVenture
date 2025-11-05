9a49c5442e3040d8babec4e24606f89d7e20f204
Zehan <zehan.kareem@gmail.com>
2025-11-05 07:17:40 +0600

    comment(iamzehan): Some comments added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Header from "@/components/home/Header/header";
 export default function Home() {
   return (
     <div className="flex flex-col dark:bg-black">
+      {/* Header Images go here */}
       <Header/>
+      {/* Header Images end here */}
     </div>
   );
 }
