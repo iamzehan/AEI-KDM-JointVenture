3e1ca049462f72f806dd277ddd3bc4c5cd7b69a2
Zehan <zehan.kareem@gmail.com>
2025-11-05 21:55:27 +0600

    update(iamzehan): Minor style changes

--- src/components/Navbar/nav-bar.tsx
+++ src/components/Navbar/nav-bar.tsx
@@ -74,13 +74,13 @@ export default function Navbar() {
       {/* Mobile Menu */}
       <div
         className={clsx(
-          "absolute my-1 top-full right-1 w-50 bg-zinc-500/50 backdrop-blur-lg flex flex-col items-center shadow-lg md:hidden transform origin-top transition-all duration-500 overflow-hidden z-40 rounded-lg list-none",
+          "absolute my-1 top-full right-1 w-50 bg-zinc-500/50 backdrop-blur-lg flex flex-col items-center shadow-lg md:hidden transform origin-top-right transition-all duration-500 overflow-hidden z-40 rounded-lg list-none",
           {
             // Closed: hidden with delayed opacity if clicked
-            "opacity-0 scale-0 translate-x-100 pointer-events-none transition-delay-[200ms]":
+            "opacity-0 scale-0 pointer-events-none transition-delay-[200ms]":
               !isOpen && isClicked,
             // Regular close
-            "opacity-0 scale-0 translate-x-100 pointer-events-none transition-delay-[0ms]":
+            "opacity-0 scale-0 pointer-events-none transition-delay-[0ms]":
               !isOpen && !isClicked,
             // Open
             "opacity-100 scale-100 translate-y-0 delay-none":
