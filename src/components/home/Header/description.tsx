eddaaa6c4570a06ac66a7972c24bf40035afe021
Zehan <zehan.kareem@gmail.com>
2025-11-05 09:15:32 +0600

    update(iamzehan): Minor design changes

--- src/components/home/Header/description.tsx
+++ src/components/home/Header/description.tsx
@@ -7,7 +7,7 @@ interface Text {
 }
 export default function Description({font}: Text) {
   return (
-    <div className="my-10 px-5 flex md:flex-row flex-col gap-10 md:static text-justify h-fit w-full">
+    <div className="my-10 px-5 flex flex-col gap-10 md:static text-justify h-fit w-full">
       <p className="text-xl md:text-shadow-lg font-bold">
         {description.eng}
       </p>
