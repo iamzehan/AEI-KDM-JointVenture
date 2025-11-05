be5757091153be49576890891cd3f16c1e6c7bc2
Zehan <zehan.kareem@gmail.com>
2025-11-05 09:13:53 +0600

    update(iamzehan): Added description

--- src/lib/header.ts
+++ src/lib/header.ts
@@ -6,3 +6,8 @@ export const text:Text = {
     eng: "Powering Bangladesh’s Future: A New Era of Energy!",
     ban: "বাংলাদেশের ভবিষ্যৎ শক্তির জাগরণ: নতুন একটি যুগের সূচনা!"
 }
+
+export const description:Text = {
+    eng: "AEI + KDM JV is building Bangladesh's first deep-sea LPG Terminal and Bulk Storage Facility, a groundbreaking project that promises reliable, affordable, and sustainable energy for every home, business, and industry.",
+    ban: "AEI + KDM JV বাংলাদেশে প্রথম গভীর সমুদ্র এলপিজি টার্মিনাল ও বাল্ক স্টোরেজ ফ্যাসিলিটি তৈরি করছে, একটি যুগান্তকারী প্রকল্প যা প্রতিটি বাড়ি, ব্যবসা এবং শিল্পের জন্য নির্ভরযোগ্য, সাশ্রয়ী, এবং টেকসই শক্তি সরবরাহের প্রতিশ্রুতি দেয়।"
+}
