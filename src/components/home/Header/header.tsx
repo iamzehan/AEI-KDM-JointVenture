9f7b1e67418a1dc2670b87d39124432be400d2d6
Zehan <zehan.kareem@gmail.com>
2025-11-05 07:15:46 +0600

    feat(iamzehan): Added Heading text

--- src/components/home/Header/header.tsx
+++ src/components/home/Header/header.tsx
@@ -1,46 +1,13 @@
 "use client";
-import Image from "next/image";
-import useEmblaCarousel from "embla-carousel-react";
-import { useEffect } from "react";
-
-import header1 from "./images/header-1.jpg";
-import header2 from "./images/header-2.jpg";
-import header3 from "./images/header-3.jpg";
-
-const images = [
-  { path: header1, alt: "Header Img 1" },
-  { path: header2, alt: "Header Img 2" },
-  { path: header3, alt: "Header Img 3" },
-];
+import Carousel from "./carousel";
+import Heading from "./heading";
 
 export default function Header() {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, dragFree: true });
-
-  // Auto-play
-  useEffect(() => {
-    if (!emblaApi) return;
-
-    const interval = setInterval(() => {
-      emblaApi.scrollNext();
-    }, 3000); // slides every 3 seconds
-
-    return () => clearInterval(interval); // cleanup
-  }, [emblaApi]);
 
   return (
-    <div className="embla overflow-hidden relative md:bg-transparent md:shadow-lg shadow-lime-600/20" ref={emblaRef}>
-      <div className="flex embla__container">
-        {images.map((image, index) => (
-          <div key={index} className="relative flex-[0_0_100%] h-64 md:h-120">
-            <Image
-              src={image.path}
-              alt={image.alt}
-              fill
-              className="object-cover md:shadow-lg"
-            />
-          </div>
-        ))}
-      </div>
+    <div className="relative flex flex-col h-full">
+      <Carousel/>
+      <Heading/>
     </div>
   );
 }
---
+++ src/components/home/Header/heading.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { Noto_Sans_Bengali } from "next/font/google";
+import clsx from "clsx";
+import { text } from "@/lib/header";
+
+
+const notoSansBengali = Noto_Sans_Bengali({
+  weight: ["400", "700"], // Specify desired weights
+  subsets: ["bengali"], // Crucial for Bangla character support
+  variable: "--font-noto-sans-bengali", // Optional CSS variable
+  display: "swap",
+});
+
+export default function Heading() {
+  return (
+    <div className="md:absolute z-12 h-fit w-full top-0 left-0 bg-zinc-950 md:top-auto md:bottom-0 md:left-0 p-3 flex flex-col md:backdrop-blur-xs md:bg-zinc-950/60">
+      <p className="text-2xl text-lime-600 md:text-white md:text-shadow-lg font-bold">
+        {text.eng}
+      </p>
+      <p
+        className={clsx(
+          [notoSansBengali.className],
+          ["text-blue-400 md:text-xl md:text-white md:text-shadow-lg"]
+        )}
+      >
+        {text.ban}
+      </p>
+    </div>
+  );
+}
