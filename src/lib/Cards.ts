95cf66e4384838e76cd390d98165358c59580253
Zehan <zehan.kareem@gmail.com>
2025-11-05 21:01:06 +0600

    feat(iamzehan): Added card data

---
+++ src/lib/Cards.ts
@@ -0,0 +1,19 @@
+interface Card{
+    title: string;
+    subtitle: string;
+}
+
+export const cards:Card[] = [
+    {
+        title: "500,000",
+        subtitle: "MT Storage Capacity"
+    },
+    {
+        title: "150 acres",
+        subtitle: "Prime Moheshkhali Land"
+    },
+    {
+        title: "Deep-water",
+        subtitle: "VLGC-capable Jetty"
+    }
+]
