e147f364731036c3b83b53711ae9797aba033a91
Zehan <zehan.kareem@gmail.com>
2025-11-05 09:14:04 +0600

    update(iamzehan): Added description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Header from "@/components/home/Header/header";
+import Description from "@/components/home/Header/description";
+import { tiroBangla } from "@/lib/fonts";
 
 export default function Home() {
   return (
-    <div className="flex flex-col dark:bg-black">
+    <div className="flex flex-col gap-4 dark:bg-black">
       {/* Header Images go here */}
-      <Header/>
-      {/* Header Images end here */}
+      <Header font={tiroBangla}/>
+      {/* Header Images end */}
+
+      {/* Description go here */}
+      <Description font={tiroBangla}/>
+      {/* Description end */}
     </div>
   );
 }
