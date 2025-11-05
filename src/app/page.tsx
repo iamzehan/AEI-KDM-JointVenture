ecc38ee7865ba9a68fdbc96051284b6dfdf458c3
Zehan <zehan.kareem@gmail.com>
2025-11-05 21:37:18 +0600

    feat(iamzehan): Added Card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 import Header from "@/components/home/Header/header";
 import Description from "@/components/home/description";
 import { tiroBangla } from "@/lib/fonts";
+import Cards from "@/components/home/Cards";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 dark:bg-black">
       {/* Header Images go here */}
-      <Header font={tiroBangla}/>
+      <Header font={tiroBangla} />
       {/* Header Images end */}
 
       {/* Description go here */}
-      <Description font={tiroBangla}/>
+      <Description font={tiroBangla} />
       {/* Description end */}
+
+      {/* Cards go here */}
+      <Cards />
+      {/* Cards end here */}
     </div>
   );
 }
--- src/components/home/Cards.tsx
+++ src/components/home/Cards.tsx
@@ -1,4 +1,27 @@
+"use client";
+import { cards } from "@/lib/Cards";
 
-export default function Cards (){
-
+export default function Cards() {
+  return (
+    <div className="
+    // mobile
+    p-2 flex flex-col flex-grow gap-3 w-8/10
+    // others 
+    md:p-5 md:gap-5 md:flex-row self-center justify-center">
+      {cards.map((card, index) => {
+        return (
+          <span key={index} className="
+          text-blue-950
+          hover:cursor-pointer
+          hover:text-lime-600
+          p-5 rounded-lg shadow-lg/20 shadow-zinc-300
+          border-t-1 border-zinc-300 hover:border-lime-600
+          hover:shadow-lime-600 transition-all duration-300 ease">
+            <p className="font-bold">{card.title}</p>
+            <p className="text-xs text-gray-500">{card.subtitle}</p>
+          </span>
+        );
+      })}
+    </div>
+  );
 }
