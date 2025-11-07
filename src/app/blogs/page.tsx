2149c37f6e844a3e9aa14a00141839dd4954d8ff
Zehan <zehan.kareem@gmail.com>
2025-11-07 06:18:47 +0600

    feat!(iamzehan): Blog page added to the main app

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,5 +1,24 @@
-export default function Blogs(){
+"use client";
+import Blogs from "@/components/Blogs";
+import { useEffect, useState, Suspense } from "react";
+export default function Page() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("api/blogs");
+      const jsonData = await response.json();
+      setData(jsonData);
+    }
+    fetchData();
+  }, []);
+  if (!data) {
+    return <div className="w-full h-screen flex justify-center items-center">Fetching...</div>;
+  } else {
     return (
-        <p className="text-red"> Our Blogs </p>
-    )
+      <div className="flex flex-col gap-4">
+        <p className="text-2xl font-extrabold">Blogs</p>
+        <Blogs data={data} />
+      </div>
+    );
+  }
 }
