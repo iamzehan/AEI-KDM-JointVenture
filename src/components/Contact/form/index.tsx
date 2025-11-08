fdeaad34d3f3c7ddd9edec63103564be92df7f47
Zehan <zehan.kareem@gmail.com>
2025-11-08 07:15:51 +0600

    fix!(iamzehan): Added some responsive styles

--- src/components/Contact/form/index.tsx
+++ src/components/Contact/form/index.tsx
@@ -4,7 +4,7 @@ export default function Form() {
   return (
     <form className="
     py-10 flex flex-col gap-3 md:gap-0 p-5 w-full xl:w-2/3 md:items-center
-    md:bg-white xl:bg-transparent
+    md:bg-white dark:bg-transparent xl:bg-transparent
     ">
       <Fields />
     </form>
