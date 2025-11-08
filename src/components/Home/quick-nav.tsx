fbcd5145d7aee33029134268661496be8b3025bd
Zehan <zehan.kareem@gmail.com>
2025-11-08 11:09:17 +0600

    fix(iamzehan): Fixed responsiveness for small devices

--- src/components/Home/quick-nav.tsx
+++ src/components/Home/quick-nav.tsx
@@ -13,11 +13,10 @@ export default function QuickNav() {
         rounded-full
         bottom-0 top-auto dark:bg-zinc-950/80 
         backdrop-blur-xs flex justify-between 
-        self-center gap-5
+        self-center gap-2 w-fit xl:gap-2 md:gap-5
         inset-shadow-2xs inset-shadow-zinc-800/20
         shadow-lg
         text-xs font-bold
-
         md:bg-white
         md:dark:bg-zinc-950
         md:rounded-xl
@@ -26,14 +25,14 @@ export default function QuickNav() {
       <Link href="/projects" onClick={handleLinkClick}>
         <span
           className="
-        text-white shadow-md
+        text-white
         transition-color
         duration-500 
-        bg-zinc-950 border-1 border-zinc-500 rounded-lg
+        bg-zinc-950 
         hover:text-lime-600 
         hover:border-lime-600 
         px-5 py-3 
-        "
+        border rounded-lg"
         >
           Explore Projects <Icon name='Cases' className="animate-bounce text-green-400" size='small'/>
         </span>
@@ -41,9 +40,7 @@ export default function QuickNav() {
       <Link href="/contact" onClick={handleLinkClick}>
         <span
         className="
-        text-white shadow-md
-        transition-color
-        duration-500 
+        text-white
         bg-lime-600
         px-5 py-2 
         rounded
